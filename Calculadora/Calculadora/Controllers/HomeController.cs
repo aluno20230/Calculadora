491f357dacd3edf15bc89984063a75235345ac57
aluno20230 <aluno20230@ipt.pt>
2022-03-17 21:50:27 +0000

    adicção das funcionalidades da virgula + inversão do numero referente aos algarismos

--- Calculadora/Calculadora/Controllers/HomeController.cs
+++ Calculadora/Calculadora/Controllers/HomeController.cs
@@ -13,8 +13,46 @@ namespace Calculadora.Controllers
             _logger = logger;
         }
 
+        [HttpGet] //esta anotação é facultativa
         public IActionResult Index()
         {
+            //inicializar a calculadora
+            ViewBag.Visor = "0";
+            return View();
+        }
+
+        [HttpPost] //mas esta anotação já é obrigatória, para o método 'escutar' o HTTP POST
+        public IActionResult Index(string botao, string visor)
+        {
+            switch (botao)
+            {
+                case "0":
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "6":
+                case "7":
+                case "8":
+                case "9":
+                    //atribuir ao visor o algarismo seleccionado
+                    if(visor!="0") visor=visor + botao;
+                    else { visor = botao; }
+                    break;
+                case ",":
+                    if (!visor.Contains(",")) visor += ",";
+                    break;
+                case "+/-":
+                    //visor -123
+                    if (visor.StartsWith("-")) visor = visor.Substring(1);
+                    else visor = "-" + visor;
+                    break;
+
+            }
+            //preparar dados a serem enviados para a view
+            ViewBag.Visor=visor;
+
             return View();
         }
 
